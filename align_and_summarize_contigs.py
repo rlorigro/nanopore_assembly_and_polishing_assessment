29284dadf4da7f4d7f06e4052ed98a70cb58598c
ryan <rlorigro@ucsc.edu>
2019-02-13 16:11:05 -0800

    added support for centromere, gap, segmental dupe BED tables

--- align_and_summarize_contigs.py
+++ align_and_summarize_contigs.py
@@ -4,7 +4,7 @@ from modules.align import *
 import argparse
 
 
-def main(ref_sequence_path, reads_sequence_path, output_dir=None):
+def main(ref_sequence_path, reads_sequence_path, minimap_preset, output_dir=None):
     if output_dir is None:
         output_dir = "./"
     else:
@@ -12,14 +12,15 @@ def main(ref_sequence_path, reads_sequence_path, output_dir=None):
 
     output_sam_file_path, output_bam_file_path = align_minimap(output_dir=output_dir,
                                                                ref_sequence_path=ref_sequence_path,
-                                                               reads_sequence_path=reads_sequence_path)
+                                                               reads_sequence_path=reads_sequence_path,
+                                                               preset=minimap_preset)
 
     process_bam(bam_path=output_bam_file_path, reference_path=ref_sequence_path, output_dir=output_dir)
 
 
 if __name__ == "__main__":
     '''
-    Processes arguments and performs tasks to generate the pileup.
+    Processes arguments
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -40,7 +41,15 @@ if __name__ == "__main__":
         required=False,
         help="desired output directory path (will be created during run time if doesn't exist)"
     )
+    parser.add_argument(
+        "--minimap_preset",
+        type=str,
+        default="map-ont",
+        choices=["map-ont", "asm5", "asm10", "asm20"],
+        required=False,
+        help="desired output directory path (will be created during run time if doesn't exist)"
+    )
 
     args = parser.parse_args()
 
-    main(reads_sequence_path=args.sequences, ref_sequence_path=args.ref, output_dir=args.output_dir)
+    main(reads_sequence_path=args.sequences, ref_sequence_path=args.ref, minimap_preset=args.minimap_preset, output_dir=args.output_dir)
